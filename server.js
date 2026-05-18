db9b9b13b6cbd70764d42e261d8a9c4b4a3819a4
webdeveloperrumsonofficial-sys <webdeveloper.rumson.official@gmail.com>
2026-05-18 14:51:17 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,11 +15,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from root
-app.use(express.static(__dirname));
+app.use('/', express.static(__dirname));
 
 // Home Route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/style.css', (req, res) => {
+    res.sendFile(path.join(__dirname, 'style.css'));
 });
 
 // API Check Route
